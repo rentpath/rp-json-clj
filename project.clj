3c742bf8b52374c4896a239c13b89501c647a3e7
Jenkins Continous Integration <jenkins@jenkins-slave-02.ci.atl.primedia.com>
2016-10-05 17:26:11 -0400

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-json-clj "0.2.0"
+(defproject com.rentpath/rp-json-clj "0.2.1-SNAPSHOT"
   :description "Miscellaneous JSON utilities"
   :url "https://github.com/rentpath/rp-json-clj"
   :license {:name "Eclipse Public License"
