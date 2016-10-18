57279f69c1d86a270044b5a4f356e7cdc393d70c
Scott McLeod <halcyonblue@gmail.com>
2016-10-18 16:13:24 -0400

    [SM][000000] Add geo-json helpers

--- src/rp/json/geo_json.clj
+++ src/rp/json/geo_json.clj
@@ -1,2 +1,15 @@
 (ns rp.json.geo-json
-  "GeoJSON utilities")
+  (:require [rp.util.string :as util-string]))
+
+(defn geo-json-feature
+  [{:keys [longitude latitude properties]}]
+  {:type "Feature"
+   :geometry {:type "Point"
+              :coordinates [(util-string/parse-double longitude)
+                            (util-string/parse-double latitude)]}
+   :properties properties})
+
+(defn geo-json-feature-collection
+  [features]
+  {:type "FeatureCollection"
+   :features features})
