c69b5834f9dc09d0b911a06ff2a04eac6ec524fa
Scott McLeod <halcyonblue@gmail.com>
2016-10-19 15:54:24 -0400

    [SM][000000] Rename geo-json helper functions

--- src/rp/json/geo_json.clj
+++ src/rp/json/geo_json.clj
@@ -1,14 +1,13 @@
-(ns rp.json.geo-json
-  (:require [rp.util.string :as util-string]))
+(ns rp.json.geo-json)
 
-(defn geo-json-feature
+(defn feature
   [{:keys [longitude latitude properties]}]
   {:type "Feature"
    :geometry {:type "Point"
               :coordinates [longitude latitude]}
    :properties properties})
 
-(defn geo-json-feature-collection
+(defn feature-collection
   [features]
   {:type "FeatureCollection"
    :features features})
